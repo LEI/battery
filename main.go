a4926c7b6bcb06616d513cf79cab3e1faafbeefa
Guillaume Frichet <guillaume.frichet@gmail.com>
2017-03-11 01:45:04 +0100

    lc

--- main.go
+++ main.go
@@ -1,24 +1,3 @@
-// battery
-// Copyright (C) 2016 Karol 'Kenji Takahashi' Woźniak
-//
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
-// OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 package main
 
 import (
@@ -42,6 +21,7 @@ var (
 func init() {
 	pflag.BoolVarP(&colorOutput, "color", "c", colorOutput, "Enable color output")
 	pflag.BoolVarP(&tmuxOutput, "tmux", "t", tmuxOutput, "Enable tmux status bar colors")
+	// pflag.StringVarP(&outputSep, "new-line", "n", outputSep, "Lines separator")
 	// pflag.BoolVarP(&asciiBar, "ascii", "a", asciiBar, "Enable ascii bar left to percentage")
 	// pflag.IntVarP(&limit, "limit", "l", limit, "Limit lines")
 }
