5443a7f61d9e10ae7324451785318b3ea7eb8641
Guillaume Frichet <guillaume.frichet@gmail.com>
2017-03-12 04:32:48 +0100

    t

--- colors.go
+++ colors.go
@@ -5,20 +5,20 @@ import (
 )
 
 var (
-	// DefaultColor used to reset foreground and background
-	DefaultColor  = "default"
+	// DefaultColor to reset foreground and background
+	DefaultColor = "default"
 	// EmptyColor when empty battery charge
-	EmptyColor    = "red"
+	EmptyColor = "red"
 	// FullColor when full battery charge
-	FullColor     = "green"
+	FullColor = "green"
 	// ChargingColor when plugged in
 	ChargingColor = "green"
-	// HighColor used when discharging, over 75%
-	HighColor     = "none"
-	// MediumColor used when discharging, between 25 and 75%
-	MediumColor   = "yellow"
-	// Lowolor used when discharging, below 25%
-	LowColor      = "red"
+	// HighColor when discharging, over 75%
+	HighColor = "none"
+	// MediumColor when discharging, between 25 and 75%
+	MediumColor = "yellow"
+	// LowColor when discharging, below 25%
+	LowColor = "red"
 )
 
 var colorMap = map[string]string{
@@ -30,12 +30,14 @@ var colorMap = map[string]string{
 	"white":   "0;37",
 }
 
+// Colors adapter
 type Colors interface {
 	Get(key string) string
 }
 
 type asciiColors struct{}
 
+// Get ASCII colors
 func (c *asciiColors) Get(key string) string {
 	clr, ok := colorMap[key]
 	if !ok {
@@ -46,6 +48,7 @@ func (c *asciiColors) Get(key string) string {
 
 type tmuxColors struct{}
 
+// Get Tmux xolors
 func (c *tmuxColors) Get(key string) string {
 	return key
 }
